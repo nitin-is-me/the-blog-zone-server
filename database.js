97b562528de7c794c72c2cd0245acef26019952e
Nitin <nitinjha2609@gmail.com>
2025-01-17 10:38:46 +0530

    import pg

--- database.js
+++ database.js
@@ -1,7 +1,9 @@
 const { Sequelize } = require("sequelize");
+const pg = require("pg");
 
 const sequelize = new Sequelize(process.env.POSTGRES_URI, {
   dialect: "postgres",
+  dialectModule: pg,
   logging: false
 });
 
